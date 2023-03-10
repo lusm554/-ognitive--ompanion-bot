bed6d6212be11e8965f488beeb53166eb78775c9
lusm <loveyousomuch554@gmail.com>
2023-03-11 02:28:36 +0300

    example of testing

---
+++ bin/test.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# bot - directory from import modules. This method enable your imports and packages as they are.
+PYTHONPATH=$PYTHONPATH:bot pytest
---
+++ test/test_example.py
@@ -0,0 +1,6 @@
+import pytest
+from bot.controller.example_controller import ChatBotController
+
+def test_smth():
+  assert 1 == 1  
+
