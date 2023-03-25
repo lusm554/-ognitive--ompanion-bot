fc64144ef73dba17b19d7f7992d74d7e4cef244c
lusm <loveyousomuch554@gmail.com>
2023-03-26 00:19:21 +0300

    Working with Database Metadata

--- examples/alchemy-database-metadata.py
+++ examples/alchemy-database-metadata.py
@@ -24,8 +24,43 @@ address_table = Table(
 	Column("email_address", String, nullable=False),
 )
 
+# for Core example
 metadata_obj.create_all(engine)
 
 
+from sqlalchemy.orm import DeclarativeBase
 
+# for MetaData object
+class Base(DeclarativeBase):
+	pass
 
+from typing import List
+from typing import Optional
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import relationship
+
+# If work with ORM
+# Declarative Mapping 
+class User(Base):
+	__tablename__ = "user_account"
+	id: Mapped[int] = mapped_column(primary_key=True)
+	name: Mapped[str] = mapped_column(String(30))
+	fullname: Mapped[Optional[str]]
+	addresses: Mapped[List["Address"]] = relationship(back_populates="user")
+	def __repr__(self) -> str:
+		return f"User(id={self.id!r}, name={self.name!r}, fullname={self.fullname!r})"
+
+# If work with ORM
+# Declarative Mapping 
+class Address(Base):
+	__tablename__ = "address"
+	id: Mapped[int] = mapped_column(primary_key=True)
+	email_address: Mapped[str]
+	user_id = mapped_column(ForeignKey("user_account.id"))
+	user: Mapped[User] = relationship(back_populates="addresses")
+	def __repr__(self) -> str:
+		return f"Address(id={self.id!r}, email_address={self.email_address!r})"
+
+# for ORM example
+Base.metadata.create_all(engine)
