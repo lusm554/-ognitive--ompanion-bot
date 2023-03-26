549adceabcfd2dca954871dec85857c81337b3ea
lusm <loveyousomuch554@gmail.com>
2023-03-26 16:05:46 +0300

    changed tabs to spaces

--- examples/alchemy-database-metadata.py
+++ examples/alchemy-database-metadata.py
@@ -8,20 +8,20 @@ metadata_obj = MetaData()
 
 # If work directly to Core
 user_table = Table(
-	"user_account",
-	metadata_obj,
-	Column("id", Integer, primary_key=True),
-	Column("name", String(30)),
-	Column("fullname", String),
+  "user_account",
+  metadata_obj,
+  Column("id", Integer, primary_key=True),
+  Column("name", String(30)),
+  Column("fullname", String),
 )
 
 # If work directly to Core
 address_table = Table(
-	"address",
-	metadata_obj,
-	Column("id", Integer, primary_key=True),
-	Column("user_id", ForeignKey("user_account.id"), nullable=False),
-	Column("email_address", String, nullable=False),
+  "address",
+  metadata_obj,
+  Column("id", Integer, primary_key=True),
+  Column("user_id", ForeignKey("user_account.id"), nullable=False),
+  Column("email_address", String, nullable=False),
 )
 
 # for Core example
@@ -32,7 +32,7 @@ from sqlalchemy.orm import DeclarativeBase
 
 # for MetaData object
 class Base(DeclarativeBase):
-	pass
+  pass
 
 from typing import List
 from typing import Optional
@@ -43,24 +43,24 @@ from sqlalchemy.orm import relationship
 # If work with ORM
 # Declarative Mapping 
 class User(Base):
-	__tablename__ = "user_account"
-	id: Mapped[int] = mapped_column(primary_key=True)
-	name: Mapped[str] = mapped_column(String(30))
-	fullname: Mapped[Optional[str]]
-	addresses: Mapped[List["Address"]] = relationship(back_populates="user")
-	def __repr__(self) -> str:
-		return f"User(id={self.id!r}, name={self.name!r}, fullname={self.fullname!r})"
+  __tablename__ = "user_account"
+  id: Mapped[int] = mapped_column(primary_key=True)
+  name: Mapped[str] = mapped_column(String(30))
+  fullname: Mapped[Optional[str]]
+  addresses: Mapped[List["Address"]] = relationship(back_populates="user")
+  def __repr__(self) -> str:
+    return f"User(id={self.id!r}, name={self.name!r}, fullname={self.fullname!r})"
 
 # If work with ORM
 # Declarative Mapping 
 class Address(Base):
-	__tablename__ = "address"
-	id: Mapped[int] = mapped_column(primary_key=True)
-	email_address: Mapped[str]
-	user_id = mapped_column(ForeignKey("user_account.id"))
-	user: Mapped[User] = relationship(back_populates="addresses")
-	def __repr__(self) -> str:
-		return f"Address(id={self.id!r}, email_address={self.email_address!r})"
+  __tablename__ = "address"
+  id: Mapped[int] = mapped_column(primary_key=True)
+  email_address: Mapped[str]
+  user_id = mapped_column(ForeignKey("user_account.id"))
+  user: Mapped[User] = relationship(back_populates="addresses")
+  def __repr__(self) -> str:
+    return f"Address(id={self.id!r}, email_address={self.email_address!r})"
 
 # for ORM example
 Base.metadata.create_all(engine)
