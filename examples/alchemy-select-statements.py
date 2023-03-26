1aef7c2c40e83f3e97f6a3689585a26d07bec0b1
lusm <loveyousomuch554@gmail.com>
2023-03-26 16:05:03 +0300

    spaces here

--- examples/alchemy-select-statements.py
+++ examples/alchemy-select-statements.py
@@ -13,28 +13,28 @@ engine = create_engine("sqlite+pysqlite:///:memory:", echo=True)
 
 # for MetaData object
 class Base(DeclarativeBase):
-	pass
+  pass
 
 # If work with ORM
 # Declarative Mapping 
 class User(Base):
-	__tablename__ = "user_account"
-	id: Mapped[int] = mapped_column(primary_key=True)
-	name: Mapped[str] = mapped_column(String(30))
-	fullname: Mapped[Optional[str]]
-	addresses: Mapped[List["Address"]] = relationship(back_populates="user")
-	def __repr__(self) -> str:
-		return f"User(id={self.id!r}, name={self.name!r}, fullname={self.fullname!r})"
+  __tablename__ = "user_account"
+  id: Mapped[int] = mapped_column(primary_key=True)
+  name: Mapped[str] = mapped_column(String(30))
+  fullname: Mapped[Optional[str]]
+  addresses: Mapped[List["Address"]] = relationship(back_populates="user")
+  def __repr__(self) -> str:
+    return f"User(id={self.id!r}, name={self.name!r}, fullname={self.fullname!r})"
 
 # If work with ORM
 # Declarative Mapping 
 class Address(Base):
-	__tablename__ = "address"
-	id: Mapped[int] = mapped_column(primary_key=True)
-	email_address: Mapped[str]
-	user_id = mapped_column(ForeignKey("user_account.id"))
-	user: Mapped[User] = relationship(back_populates="addresses")
-	def __repr__(self) -> str:
+  __tablename__ = "address"
+  id: Mapped[int] = mapped_column(primary_key=True)
+  email_address: Mapped[str]
+  user_id = mapped_column(ForeignKey("user_account.id"))
+  user: Mapped[User] = relationship(back_populates="addresses")
+  def __repr__(self) -> str:
     return f"Address(id={self.id!r}, email_address={self.email_address!r})"
 
 # for ORM example
