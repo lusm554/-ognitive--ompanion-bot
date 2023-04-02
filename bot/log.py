8c041a73cde2d5b0754ace3e0c33bdda5bc38648
lusm <loveyousomuch554@gmail.com>
2023-04-02 17:09:23 +0300

    logging module

--- bot/log.py
+++ bot/log.py
@@ -1,3 +1,14 @@
-"""
-Here is the management of logging settings. 
-"""
+import logging
+from config import LoggingConfig
+
+def get_logger(name: str = None) -> logging.Logger:
+  """ Generate logger by name. """
+  logger = logging.getLogger(name)
+  logger.setLevel(LoggingConfig.LOG_LEVEL)
+  formatter = logging.Formatter(
+      f"%(asctime)s %(name)s[%(lineno)d] - %(levelname)s: %(message)s", datefmt="%m-%d-%Y %H:%M:%S %Z%z"
+  )
+  handler = logging.StreamHandler()
+  handler.setFormatter(formatter)
+  logger.addHandler(handler)
+  return logger
