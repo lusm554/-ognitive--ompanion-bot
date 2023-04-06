1cc4083b1c6999bfc3d5b7544010d2739b0181a7
lusm <loveyousomuch554@gmail.com>
2023-04-06 19:27:08 +0300

    error: cannot run init db and telegram application in async

--- bot/main.py
+++ bot/main.py
@@ -3,15 +3,25 @@ from database import DB
 from config import TelegramConfig
 from telegramfront import telegram_main
 
+async def initdb():
+  await DB.init_db()
+
 async def main():
-  global DB
-  # instance_db = await DB.init_db() # setting up tables 
-  # for AsyncEngine created in function scope, close and
-  # clean-up pooled connections
-  # await instance_db.engine.dispose()
+  # global DB
 
   # running telegram bot
-  await telegram_main(TelegramConfig.TELEGRAM_TOKEN)
+  telegram_application = telegram_main(TelegramConfig.TELEGRAM_TOKEN)
+  telegram_application.bot_data["controller"] = 1 # init here controller for bot command handlers
+
+  # starting bot in separate task
+  bot_task = asyncio.create_task(telegram_application.run_polling())
+  # starting the database initialization in a separate task
+  db_task  = asyncio.create_task(initdb())
+
+  # wait for both tasks to complete
+  await asyncio.gather(bot_task, db_task)
 
 if __name__ == "__main__":
-  telegram_main()
+  loop = asyncio.get_event_loop()
+  loop.run_until_complete(main())
+  loop.close()
